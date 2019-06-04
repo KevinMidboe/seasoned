9ec6e366773dd42a1e0c6da7902ec74d067f3595
Kevin Midboe <kevin.midboe@gmail.com>
2019-06-05 00:10:25 +0200

    Updated version in pacage file and added compression to server requests.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var path = require('path');
+const compression = require('compression')
 var history = require('connect-history-api-fallback');
 
 app = express();
 
+app.use(compression())
 app.use('/dist', express.static(path.join(__dirname + "/dist")));
 app.use('/dist', express.static(path.join(__dirname + "/dist/")));
 app.use('/favicons', express.static(path.join(__dirname + "/favicons")));
