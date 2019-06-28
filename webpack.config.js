8696d9cb16dc63f7f22349dc93e0ecbdb756ea0a
KevinMidboe <kevin.midboe@gmail.com>
2019-06-28 21:20:35 +0200

    Upgraded babel and webpack to support async keyword with preset-env over es2015

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,9 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        },
         exclude: /node_modules/
       },
       {
