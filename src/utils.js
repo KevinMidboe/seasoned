431cb7c034fb68f550507d9ed158165d658b2e63
KevinMidboe <kevin.midboe@gmail.com>
2019-10-04 17:44:17 +0200

    sortableSize helper function re-done to be a async function

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,11 @@
-function sortableSize(string) {
+const sortableSize = (string) => {
 	const UNITS = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 	const [numStr, unit] = string.split(' ');
-	if (UNITS.indexOf(unit) === -1)
+
+  if (UNITS.indexOf(unit) === -1)
 	  return string
-	const exponent = UNITS.indexOf(unit) * 3
+
+  const exponent = UNITS.indexOf(unit) * 3
 	return numStr * (Math.pow(10, exponent))
 }
 
