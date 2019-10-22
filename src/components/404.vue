9bb98ce56996f618947e028030b8107e96be009e
KevinMidboe <kevin.midboe@gmail.com>
2019-10-22 22:53:51 +0200

    Removed unused script element and updated positioning of text to center, even on mobile.

--- src/components/404.vue
+++ src/components/404.vue
@@ -4,41 +4,34 @@
   </section>
 </template>
 
-<script>
-import storage from '../storage.js'
-export default {
-  created(){
-    document.title = 'Page Not Found' + storage.pageTitlePostfix;
-  }
-}
-</script>
-
 <style lang="scss" scoped>
 @import "./src/scss/variables";
 @import "./src/scss/media-queries";
+
 .not-found {
-  width: 100%;
+  display: flex;
   height: calc(100vh - var(--header-size));
+  width: 100%;
   background: url('~assets/pulp-fiction.jpg') no-repeat 50% 50%;
   background-size: cover;
-  display: flex;
   justify-content: center;
 
   &:before {
-    background: $background-40;
-    height: calc(100vh - var(--header-size));
-    width: 100%;
     content: "";
     position: absolute;
+    height: calc(100vh - var(--header-size));
+    width: 100%;
+    background: $background-40;
   }
-  &__title{
+  &__title {
    padding-top: 40vh;
     font-size: 2rem;
     font-weight: 500;
     color: $text-color;
     position: relative;
     margin: 0;
-    @include tablet-min{
+
+    @include tablet-min {
       font-size: 2.3rem;
     }
   }
