a0810fbee1987b49d4a6b284849f60cc05f60bdc
Kevin Midboe <kevin.midboe@gmail.com>
2022-03-04 18:34:48 +0100

    Linting

--- src/modules/documentTitle.js
+++ src/modules/documentTitle.js
@@ -1,41 +1,48 @@
-const capitalize = (string) => {
-  return string.includes(' ') ? 
-    string.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).replace('_', ' ')).join(' ')
-    : string.charAt(0).toUpperCase() + string.slice(1)
-}
+const capitalize = string => {
+  if (!string) return;
 
-const setDocumentTitle = (state) => {
-  document.title = `${state.emoji} ${state.titlePrefix} | ${capitalize(state.title)}`
-}
+  return string.includes(" ")
+    ? string
+        .split(" ")
+        .map(
+          word => word.charAt(0).toUpperCase() + word.slice(1).replace("_", " ")
+        )
+        .join(" ")
+    : string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const setDocumentTitle = state => {
+  document.title = `${state.emoji} ${state.titlePrefix} | ${capitalize(
+    state.title
+  )}`;
+};
 
 export default {
   namespaced: true,
   state: {
-    emoji: '',
-    titlePrefix: 'seasoned',
+    emoji: "",
+    titlePrefix: "seasoned",
     title: undefined
   },
   getters: {
-    title: (state) => {
-      return state.title
-    }
+    title: state => state.title
   },
   mutations: {
     SET_EMOJI: (state, emoji) => {
-      state.emoji = emoji
-      setDocumentTitle(state)
+      state.emoji = emoji;
+      setDocumentTitle(state);
     },
     SET_TITLE: (state, title) => {
-      state.title = title
-      setDocumentTitle(state)
+      state.title = title;
+      setDocumentTitle(state);
     }
   },
   actions: {
     updateEmoji({ commit }, emoji) {
-      commit('SET_EMOJI', emoji)
+      commit("SET_EMOJI", emoji);
     },
     updateTitle({ commit }, title) {
-      commit('SET_TITLE', title)
+      commit("SET_TITLE", title);
     }
   }
-}
+};
--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,26 @@
-const sortableSize = (string) => {
-	const UNITS = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-	const [numStr, unit] = string.split(' ');
+const sortableSize = string => {
+  const UNITS = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const [numStr, unit] = string.split(" ");
 
-  if (UNITS.indexOf(unit) === -1)
-	  return string
+  if (UNITS.indexOf(unit) === -1) return string;
 
-  const exponent = UNITS.indexOf(unit) * 3
-	return numStr * (Math.pow(10, exponent))
+  const exponent = UNITS.indexOf(unit) * 3;
+  return numStr * Math.pow(10, exponent);
 };
 
-const parseJwt = (token) => {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+const parseJwt = token => {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
 
-    return JSON.parse(jsonPayload);
+  return JSON.parse(jsonPayload);
 };
 
-
-export { sortableSize, parseJwt }
+export { sortableSize, parseJwt };
