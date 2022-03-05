25dd8bea9eb29a2e280b980a4df11006998a1636
Kevin Midboe <kevin.midboe@gmail.com>
2022-03-05 13:06:38 +0100

    Updated and removed unused packages

--- src/main.js
+++ src/main.js
@@ -1,24 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import axios from "axios";
 import router from "./routes";
 import store from "./store";
 
 import Toast from "./plugins/Toast";
-import DataTablee from "vue-data-tablee";
-import VModal from "vue-js-modal";
-
 import App from "./App.vue";
 
 window.eventHub = new Vue();
 
 Vue.use(VueRouter);
 Vue.use(Toast);
-Vue.use(DataTablee);
-Vue.use(VModal, { dialog: true });
 
 store.dispatch("darkmodeModule/findAndSetDarkmodeSupported");
-store.dispatch("user/initFromLocalStorage");
+store.dispatch("user/initUserFromCookie");
 
 new Vue({
   el: "#app",
