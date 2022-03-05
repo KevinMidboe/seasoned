d58504cde3052a2035cf1f5b083d1d567dc0e3ec
Kevin Midboe <kevin.midboe@gmail.com>
2022-03-05 09:06:35 +0100

    Some weird fill & transition-duration hacks for color animation.

--- src/icons/IconClose.vue
+++ src/icons/IconClose.vue
@@ -1,19 +1,15 @@
 <template>
   <svg
+    id="icon-cross"
+    viewBox="0 0 32 32"
+    xmlns="http://www.w3.org/2000/svg"
     @click="$emit('click')"
     @keydown="event => $emit('keydown', event)"
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    class="feather feather-x"
+    style="transition-duration: 0s;"
   >
-    <line x1="18" y1="6" x2="6" y2="18"></line>
-    <line x1="6" y1="6" x2="18" y2="18"></line>
+    <path
+      fill="inherit"
+      d="M27.942 5.942l-1.883-1.883-10.058 10.054-10.058-10.054-1.883 1.883 10.054 10.058-10.054 10.058 1.883 1.883 10.058-10.054 10.058 10.054 1.883-1.883-10.054-10.058z"
+    ></path>
   </svg>
 </template>
