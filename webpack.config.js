6642b2531ea696c1f343eb86ba02e800fbeb92cd
Kevin Midboe <kevin.midboe@gmail.com>
2022-08-14 21:25:09 +0200

    Test node_modules caching

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,8 @@ Object.keys(process.env).forEach(key => {
   }
 });
 
+console.log("WEBPACK variables:", variables, process.env);
+
 module.exports = {
   mode: process.env.NODE_ENV,
   context: publicPath,
