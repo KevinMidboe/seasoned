fb31698ce8939d890e06e0e227b5626e6724c232
Kevin Midboe <kevin.midboe@gmail.com>
2026-02-24 00:20:59 +0100

    remove nginx config - replaced by caddy

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-export SEASONED_API=${SEASONED_API:-http://localhost:31459}
-export SEASONED_DOMAIN=${SEASONED_DOMAIN:-localhost}
-
-envsubst '$SEASONED_API,$SEASONED_DOMAIN' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-exec "$@"
