04138d30ac8abf6811817b608c15222bb65a2fef
Kevin Midboe <kevin.midboe@gmail.com>
2026-03-08 21:05:38 +0100

    Add storage management components for data & privacy section
    - Create StorageManager component for browser storage overview
    - Create StorageSectionBrowser for localStorage/sessionStorage/cookies
    - Create StorageSectionServer for server-side data (mock)
    - Create ExportSection for data export functionality
    - Refactor DataExport component with modular sections
    - Add storage icons (IconCookie, IconDatabase, IconTimer)
    - Implement collapsible sections with visual indicators
    - Add colored borders per storage type
    - Display item counts and total size in headers

---
+++ src/icons/IconDatabase.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    id="icon-database"
+    viewBox="0 0 24 24"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    style="transition-duration: 0s"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    @click="$emit('click')"
+    @keydown="event => $emit('keydown', event)"
+  >
+    <path
+      d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"
+    />
+  </svg>
+</template>
